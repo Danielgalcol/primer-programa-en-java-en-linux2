725b280f45960c51d3ca5bad2cebca4da5c3018b
Xubuntu <danielgalcol@gmail.com>
2023-10-11 13:47:53 +0200

    2ºcommit/ mi lugar favorito mas mi deporte

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ public class Prueba{
 	public static void main(String arg[]){
 	
 	System.out.println("Fútbol es mii deporte favorito");
-		
+	System.out.println("Mi lugar favorito es el Humedo");		
+	
 	}
 }
